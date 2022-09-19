bd70603a9b637f6d2a58e42b8809e15b38455436
AlbertoZMiranda <albertozmiranda1@gmail.com>
2022-09-19 11:49:04 -0500

    Creación 'petfinder' en Spring Boot y configuracion application.properties.

---
+++ petfinder/src/main/java/com/petfinder/backend/PetfinderApplication.java
@@ -0,0 +1,13 @@
+package com.petfinder.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetfinderApplication.class, args);
+	}
+
+}
---
+++ petfinder/src/test/java/com/petfinder/backend/PetfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petfinder.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
